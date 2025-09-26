b0a51a58fb12c1db57addea7b5b2bea88c5f37f2
zotel1 <zotelsigel@gmail.com>
2025-09-26 12:35:34 -0300

    sumamos el ejercicio 2 explicacion en codigo

--- primera_clase/ejercicio001.py
+++ primera_clase/ejercicio001.py
@@ -1,6 +1,14 @@
 import pandas as pd
 diet.index = pd.to_datetime(diet.index)
 
+
+"""
+cortamos el conjunto de datos diet para conservar solo slkos valores de 2012, asignandolos a diet2012
+- traza rl diet2012, creando de nuevo lineas de cuadricula con el argumento grid
+"""
+# Slice the dataset to keep only 212
+diet2012 = diet[2012]
+
 #Plot the entire time series diet and show gridlines
 diet.plot(grid=True)
 plt.show()
