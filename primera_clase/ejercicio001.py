a532855747fbdda82aaa66066bf1cab69632f4a0
zotel1 <zotelsigel@gmail.com>
2025-09-26 12:37:03 -0300

    el parametro 2012 tiene que ser pasado como string

--- primera_clase/ejercicio001.py
+++ primera_clase/ejercicio001.py
@@ -7,7 +7,7 @@ cortamos el conjunto de datos diet para conservar solo slkos valores de 2012, as
 - traza rl diet2012, creando de nuevo lineas de cuadricula con el argumento grid
 """
 # Slice the dataset to keep only 212
-diet2012 = diet[2012]
+diet2012 = diet['2012']
 
 #Plot the entire time series diet and show gridlines
 diet.plot(grid=True)
