6cecc6a2b9cfcfdff3a698af88efd7089ac27a33
zotel1 <zotelsigel@gmail.com>
2025-09-27 14:23:48 -0300

    ejercicio 4 y 5 check

---
+++ primera_clase/ejercicio004.py
@@ -0,0 +1,11 @@
+import pandas as levels
+import matplotlib.pyplot as plt
+
+# Compute correlation of levels
+correlation1 = levels['DJI'].corr(levels['UFO'])
+print("Correlation of levels: ", correlation1)
+
+# Compute correlation of percent changes
+changes = levels.pct_change()
+correlation2 = changes['DJI'].corr(changes['UFO'])
+print("Correlation of percent changes: ", correlation2)
